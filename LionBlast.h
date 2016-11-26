591723de1248b01c020d8af63733822740d6fd87
Leandro Araújo <jogos.gronkel@gmail.com>
2016-11-26 18:55:26 -0200

    Add files via upload

--- LionBlast.h
+++ LionBlast.h
@@ -1,59 +1,34 @@
 #include "todas.h"
 #include "lionphys.cpp"
-#include "mapa2.cpp"
 
 using namespace std;
 
 int _x = 0;
 int _y = 40;
 
-struct VetorInt {
-    int x;
-    int y;
-};
-
 class Canhao {
-    VetorInt S;
+    VetorBi S;
     int vidas = 5;
-    bool good = false;
 public:
     void Inicia(int x, int y) {
-        while (!good) {
-        x = rand()%57;
-        y = rand()%40;
-        if ((mapa[S.y+2][S.x] != '@') && (mapa[S.y+2][S.x+3] != '@') && (mapa[S.y+3][S.x] == '@')) {
-            good = true;
-            S.x = x;
-            S.y = y;
-        }else{
-            good = false;
-        }
+        S.x = x;
+        S.y = y;
     }
-    ImprimirArquivo("tanque.txt", S.x, S.y);
-    }
-    void DisparaBala(float v, int angulo, VetorBi s0) {
-        VetorBi coord;
-        float Vx;
-        float Vy;
-        int Xcoord = 0;
-        int Ycoord = 0;
-
+    void DisparaBala(int v, int angulo, VetorBi s0) {
+        bool out = false;
+        bool in = false;
+        int Vx;
+        int Vy;
         Vx = DecompoeVetorBi(v, angulo).x;
         Vy = DecompoeVetorBi(v, angulo).y;
-
-        for (float t = 0; mapa[Ycoord][Xcoord] != '@'; t += 0.01) {
-            coord = MovRetUniVariado(Vx, Vy, s0, t, 1000);
-            GotoXY(coord.x, coord.y);
-            cout << "*";
-            GotoXY(118,0);
-            cout << coord.x << " " << coord.y;
-            Xcoord = static_cast <int> (coord.x);
-            Ycoord = static_cast <int> (coord.y);
-            Sleep(250);
-            GotoXY(coord.x, coord.y);
-            cout << " ";
+        for (int t = 0; !out; t+=0.1) {
+            MovRetUniVariado (Vx, Vy, s0, t, 10);
+            if (s0.y == _y && in) {
+                out = true;
+            }
+            in = true;
         }
+        GotoXY(s0.x, s0.y);
+        cout << "*";
     }
-
-
 };
