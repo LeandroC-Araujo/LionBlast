266abf67bce91fa5668933cfe95b3f752ef4d69e
Leandro Araújo <jogos.gronkel@gmail.com>
2016-11-26 18:57:17 -0200

    Add files via upload

--- LionBlast.cpp
+++ LionBlast.cpp
@@ -1,32 +1,30 @@
-#include "LionBlast.h"
+#include "LionBlast_teste.h"
 
 int main ()
 
 {
     srand(time(NULL));
-    ConsoleSize(180,50);
+    ConsoleSize(124,44);
     Canhao A;
     Canhao B;
-    int xA;
-    int yA;
-    int xB;
-    int yB;
-    ImprimirArquivo("fase1.txt", 0, 0);
-    ifstream mapa;
-    string _mapa[50];
-    mapa.open("fase1.txt");
-    if(mapa.is_open() && mapa.good()) {
-        for (int j = 0; j < 50; j++) {
-            getline(mapa, _mapa[j], '\n');
+
+    for (int j = 0; j < 45; j++) {
+        for (int i = 0; i < 124; i++) {
+            cout << mapa[j][i];
         }
     }
 
-    for (int j = 0; j < 50; j++) {
-        mapa >> _mapa[j];
+    VetorBi sA;
+
+    float vA;
+    float vB;
+    A.Inicia(sA.x-3, sA.y-2);
+    sA.x = A.S.x+3;
+    sA.y = A.S.y;
+    while (1) {
+        cin >> vA;
+        A.DisparaBala(vA, 30, sA);
     }
 
-    xA = rand()%85;
-    yA = rand()%50;
 
-    ImprimirArquivo("tanque.txt", xA, yA);
 }
--- LionBlast.h
+++ LionBlast.h
@@ -1,34 +1,59 @@
 #include "todas.h"
 #include "lionphys.cpp"
+#include "mapa2.cpp"
 
 using namespace std;
 
 int _x = 0;
 int _y = 40;
 
+struct VetorInt {
+    int x;
+    int y;
+};
+
 class Canhao {
-    VetorBi S;
     int vidas = 5;
+    bool good = false;
 public:
+    VetorInt S;
     void Inicia(int x, int y) {
-        S.x = x;
-        S.y = y;
+        while (!good) {
+        x = rand()%57;
+        y = rand()%40;
+        if ((mapa[y+2][x] != '@') && (mapa[y+2][x+3] != '@') && (mapa[y+3][x] == '@')) {
+            good = true;
+            S.x = x;
+            S.y = y;
+        }else{
+            good = false;
+        }
     }
-    void DisparaBala(int v, int angulo, VetorBi s0) {
-        bool out = false;
-        bool in = false;
-        int Vx;
-        int Vy;
+    ImprimirArquivo("tanque.txt", S.x, S.y);
+    }
+    void DisparaBala(float v, int angulo, VetorBi s0) {
+        VetorBi coord;
+        float Vx;
+        float Vy;
+        int Xcoord = 0;
+        int Ycoord = 0;
+
         Vx = DecompoeVetorBi(v, angulo).x;
         Vy = DecompoeVetorBi(v, angulo).y;
-        for (int t = 0; !out; t+=0.1) {
-            MovRetUniVariado (Vx, Vy, s0, t, 10);
-            if (s0.y == _y && in) {
-                out = true;
-            }
-            in = true;
+
+        for (float t = 0; mapa[Ycoord][Xcoord] != '@'; t += 0.01) {
+            coord = MovRetUniVariado(Vx, Vy, s0, t, 1000);
+            GotoXY(coord.x, coord.y);
+            cout << "*";
+            GotoXY(118,0);
+            cout << coord.x << " " << coord.y;
+            Xcoord = static_cast <int> (coord.x);
+            Ycoord = static_cast <int> (coord.y);
+            Sleep(250);
+            GotoXY(coord.x, coord.y);
+            cout << " ";
         }
-        GotoXY(s0.x, s0.y);
-        cout << "*";
     }
+
+
 };
