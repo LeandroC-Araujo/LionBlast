7735484dfddcc976463e22e01f1e39a2f3acdd32
squanchloser <jogos.gronkel@gmail.com>
2016-12-03 09:48:09 -0200

    LionPhys atualizada (+BoundingBox, Movimento Circular Uniforme, Movimento Oblíquo )

--- lionphys.cpp
+++ lionphys.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+struct QUADRI {
+    int x;
+    int y;
+    float h;
+    float l;
+};
+
 /**\brief recebe a velocidade, posição inicial e tempo do movimento do corpo e
           retorna sua posição após descrever o Movimento Retilíneo Uniforme */
 VetorBi MovRetUniforme (float v, VetorBi s0, float t) {
@@ -16,8 +23,8 @@ VetorBi MovRetUniforme (float v, VetorBi s0, float t) {
 VetorBi MovRetUniVariado_esquerda (float Vx, float Vy, VetorBi s0, float t, float a) {
     VetorBi coord;
 
-    coord.y = s0.y - Vy*t + a*(t*t)/2;
     coord.x = s0.x + Vx*t;
+    coord.y = s0.y - Vy*t + a*(t*t)/2;
 
     return coord;
 }
@@ -27,14 +34,57 @@ VetorBi MovRetUniVariado_esquerda (float Vx, float Vy, VetorBi s0, float t, floa
 VetorBi MovRetUniVariado_direita (float Vx, float Vy, VetorBi s0, float t, float a) {
     VetorBi coord;
 
-    coord.y = s0.y - Vy*t + a*(t*t)/2;
     coord.x = s0.x - Vx*t;
+    coord.y = s0.y - Vy*t + a*(t*t)/2;
 
     return coord;
 }
 
-VetorBi MovCircularUni (float w, float t) {
+/**\brief recebe as velocidades X e Y, o vetor posição, o tempo e a aceleração e retorna a posição
+          após descrever o Movimento Oblíquo */
+VetorBi MovObliquo (float Vx, float Vy, VetorBi s0, float t, float a) {
+    VetorBi coord;
 
+    coord.x = MovRetUniforme(Vx, s0, t).x;
+    coord.y = MovRetUniVariado_esquerda(Vx, Vy, s0, t, a).y;
+
+    return coord;
+}
+
+VetorBi MovCircularUni (float w, float t, float r) {
+    VetorBi coord;
+
+    coord.x = cos(w*t)*r;
+    coord.y = sin(w*t)*r;
+}
+
+bool BoundingBox (QUADRI A, QUADRI B) {
+    float esquerda_A = A.x;
+    float direita_A = A.x + A.l;
+    float cima_A = A.y;
+    float baixo_A = A.y + A.h;
+    float esquerda_B = B.x;
+    float direita_B = B.x + B.l;
+    float cima_B = B.y;
+    float baixo_B = B.y + B.h;
+
+    if (esquerda_A > direita_B) {
+        return false;
+    }else{
+        if (direita_A < esquerda_B) {
+            return false;
+        }else{
+            if (cima_A > baixo_B) {
+                return false;
+            }else{
+                if (baixo_A < cima_B) {
+                    return false;
+                }else {
+                    return true;
+                }
+            }
+        }
+    }
 }
 
 /**\brief -Recebe as coordenadas X e Y das circunferências A e B e o raio de cada uma
